16e378c54bf7b23097f650e667f3d599bf5edd92
joeyklee <jsphknglee@gmail.com>
2019-02-10 23:15:48 -0500

    added step 2

---
+++ src/models/feelings.model.js
@@ -0,0 +1,15 @@
+// feelings-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const feelings = new Schema({
+    text: { type: String, required: true }
+  }, {
+    timestamps: true
+  });
+
+  return mongooseClient.model('feelings', feelings);
+};
