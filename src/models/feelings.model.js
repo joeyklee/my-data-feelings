af40aceae9f4c6ed504d0099fb7cbf016287bd8a
joeyklee <jsphknglee@gmail.com>
2019-02-10 23:17:22 -0500

    added feelings model

--- src/models/feelings.model.js
+++ src/models/feelings.model.js
@@ -6,7 +6,30 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
   const feelings = new Schema({
-    text: { type: String, required: true }
+    mood: { 
+        type: String, // text
+        required: true 
+    },
+    anxiety:{
+        type: Number, // scale from 1 to 10
+        required: true
+    },
+    stress:{
+        type: Number, // scale from 1 to 10
+        required: true
+    },
+    contentment:{
+        type: Number, // scale from 1 to 10
+        required: true
+    },
+    productivity:{
+        type: Number, // scale from 1 to 10
+        required: true
+    },
+    fitness:{
+        type: Boolean, // true or false
+        required: true
+    }
   }, {
     timestamps: true
   });
